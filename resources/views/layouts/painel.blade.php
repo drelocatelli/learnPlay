7b61ff80c58a5a529eddb1b843d46950f0d94c52
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-07-05 15:06:23 -0300

    Merge branch 'main' of https://github.com/drelocatelli/learnPlay

--- resources/views/layouts/painel.blade.php
+++ resources/views/layouts/painel.blade.php
@@ -11,6 +11,37 @@
     <title>LearnPlay</title>
 </head>
 <body>
+    <a href="javascript:void(0);" onclick="showMenu()" class="toggle-sidebar"><i class="fas fa-bars"></i></a>
+    <div class="sidebar  flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; display:none;">
+
+        <ul class="nav nav-pills flex-column mb-auto mt-5">
+          <li class="nav-item">
+            <a href="#" class="nav-link link-dark">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Orders
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Products
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Customers
+            </a>
+          </li>
+        </ul>
+      </div>
 
     <div class="container">
 
@@ -33,6 +64,9 @@
                     <a href="{{route('dashboard')}}" class="nav-link" title="Página inicial"><i class="fas fa-home"></i></a>
                 </li>
                 <li class="nav-item">
+                    <a href="#" class="nav-link"><i class="fas fa-bell"></i> &nbsp;<span style="font-family:sans-serif; font-size:18px;">0</span></a>
+                </li>
+                <li class="nav-item">
                     <a href="#" class="nav-link" title="Configurações"><i class="fas fa-user-edit"></i></a>
                 </li>
 
@@ -41,13 +75,30 @@
                         <i class="fas fa-times"></i>
                     </a>
                 </li>
+
+
               </ul>
         </nav>
         <div style="clear:both;"></div>
-        @yield('conteudo')
+
+          <div>
+            @yield('conteudo')
+
+          </div>
     </div>
 
+
     </div>
+    <script>
+        function showMenu(){
+            let menu = document.querySelector('.sidebar')
+            if(menu.style.display == 'block'){
+                menu.style.display = "none"
+            }else{
+                menu.style.display = "block"
+            }
+        }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
