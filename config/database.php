0e6a1de381b82611e66ec515a7bed737ec16b3c1
drelocatelli <andressalocatelli@hotmail.com>
2023-01-10 21:09:29 -0300

    adding docker

--- config/database.php
+++ config/database.php
@@ -46,8 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
